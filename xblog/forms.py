89710963af588c554039b96c8ae89202b5960989
Eric Williams <ewi09@ovp.bskyb.com>
2017-08-12 11:22:38 +0100

    Cleaned up forms.py

--- xblog/forms.py
+++ xblog/forms.py
@@ -1,13 +1,26 @@
-from django.forms import ModelForm, CharField
+"""
 
-from .models import Author
+Custom forms for xblog models
+
+
+"""
+from django.forms import ModelForm
+# from django.forms import CharField
+
+# from .models import Author
 from .models import Post
 
 class PostCreateForm(ModelForm):
+    """
+    Defines the create form for Post objects
+    """
     class Meta:
+        """
+        Meta class for admin
+        """
         model = Post
-        fields = ['title', 'body','categories','status', 'tags', 'text_filter', 'blog']
+        fields = ['title', 'body', 'categories', 'status', 'tags', 'text_filter', 'blog']
 
-        def __init__(self, *args, **kwargs):
-            super(WorklogCreateForm, self).__init__(self, *args, **kwargs)
-            self.fields['project'].queryset = Post.objects.filter(Project.status == 2)
+        # def __init__(self, *args, **kwargs):
+        #     ModelForm.__init__(self, *args, **kwargs)
+        #     self.fields['project'].queryset = Post.objects.filter(Project.status == 2)
